6684fe722178fd26e0301060560e94e81bd1c82e
WooSeok Kim <3suksw@gmail.com>
2023-05-17 13:46:47 +0900

    add route files for windows

---
+++ checkbox-to-do-app/frontend/src/routes/Login.js
@@ -0,0 +1,32 @@
+import { useEffect } from "react";
+
+const initNaverLogin = () => {
+  var naver_id_login = new window.naver_id_login(
+    "nuZ04sTeb2LDphCqc4qv", // client ID
+    "http://localhost:3000/callback" // callback url
+  );
+  var state = naver_id_login.getUniqState();
+  naver_id_login.setButton("white", 2, 40);
+  naver_id_login.setDomain("http://localhost:3000");
+  naver_id_login.setState(state);
+  naver_id_login.setPopup();
+  naver_id_login.init_naver_id_login();
+
+  console.log(naver_id_login);
+  // TODO: fetch userID, userName, and etc.
+};
+
+const Login = () => {
+  useEffect(() => {
+    initNaverLogin();
+  }, []);
+
+  return (
+    <>
+      <div>Hello</div>
+      <div id="naver_id_login"></div>
+    </>
+  );
+};
+
+export default Login;
