95f883950bd8ee92aa01797b21be2e3c1c1f81bd
WooSeok Kim <3suksw@gmail.com>
2023-05-17 13:50:49 +0900

    update

--- checkbox-to-do-app/frontend/src/App.js
+++ checkbox-to-do-app/frontend/src/App.js
@@ -1,9 +1,22 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Login from "./routes/Login";
+import Callback from "./routes/Callback";
+
 const App = () => {
-  const usrInfo = "";
   return (
-    <div className="app">
-      <h1>Checkbox ✓</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+      <Routes>
+        <Route path="/login" element={<Login />}></Route>
+      </Routes>
+      <Routes>
+        <Route path="/callback" element={<Callback />}></Route>
+      </Routes>
+    </Router>
   );
 };
 
