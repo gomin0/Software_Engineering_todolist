29b5acd56757f6a82ae53892f3bb61ee11f951e2
WooSeok Kim <3suksw@gmail.com>
2023-05-20 03:28:05 +0900

    add list onClick event function

--- checkbox-to-do-app/frontend/src/components/Main.js
+++ checkbox-to-do-app/frontend/src/components/Main.js
@@ -1,11 +1,26 @@
+import { useState } from "react";
 import style from "./Main.css";
 import SideBar from "./SideBar";
+import Inbox from "./Inbox";
 
 const Main = ({ lists }) => {
+  var initialList = lists[0];
+  var [curList, setListName] = useState(initialList);
+
+  const clickAndSelectList = (event) => {
+    const selectedID = event.target.id;
+    const selectedList = lists.find((list) => list.id === parseInt(selectedID));
+    setListName(selectedList);
+  };
+
   return (
     <div className="main" style={style}>
-      <SideBar lists={lists} />
-      <div>TODOS GO HERE</div>
+      <SideBar
+        onClick={(event) => clickAndSelectList(event)}
+        lists={lists}
+        selected={curList}
+      />
+      <Inbox curList={curList} />
     </div>
   );
 };
