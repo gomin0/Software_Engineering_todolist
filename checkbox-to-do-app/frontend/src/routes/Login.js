324f6ea3c256e321e76d1437f53bbc1337e6ccd3
WooSeok Kim <3suksw@gmail.com>
2023-05-24 00:27:49 +0900

    rm console.log()

--- checkbox-to-do-app/frontend/src/routes/Login.js
+++ checkbox-to-do-app/frontend/src/routes/Login.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 
 const initNaverLogin = () => {
-  const naver_id_login = new window.naver_id_login(
+  var naver_id_login = new window.naver_id_login(
     "nuZ04sTeb2LDphCqc4qv", // client ID
     "http://localhost:3000/" // callback url
   );
@@ -9,11 +9,8 @@ const initNaverLogin = () => {
   naver_id_login.setButton("white", 2, 40);
   naver_id_login.setDomain("http://localhost:3000");
   naver_id_login.setState(state);
-  naver_id_login.setPopup();
+  //naver_id_login.setPopup();
   naver_id_login.init_naver_id_login();
-
-  console.log(naver_id_login);
-  // TODO: fetch userID, userName, and etc.
 };
 
 const Login = () => {
