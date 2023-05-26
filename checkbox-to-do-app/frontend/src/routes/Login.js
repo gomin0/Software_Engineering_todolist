d5a0fcb175242d7e55abb22c40c6a9cea463b51e
WooSeok Kim <3suksw@gmail.com>
2023-05-26 21:33:07 +0900

    migration to NaverLogin.js

--- checkbox-to-do-app/frontend/src/routes/Login.js
+++ checkbox-to-do-app/frontend/src/routes/Login.js
@@ -1,27 +1,24 @@
 import { useEffect } from "react";
+import NaverLogin from "../NaverLogin";
 
-const initNaverLogin = () => {
-  var naver_id_login = new window.naver_id_login(
-    "nuZ04sTeb2LDphCqc4qv", // client ID
-    "http://localhost:3000/" // callback url
-  );
-  var state = naver_id_login.getUniqState();
-  naver_id_login.setButton("white", 2, 40);
-  naver_id_login.setDomain("http://localhost:3000");
-  naver_id_login.setState(state);
-  //naver_id_login.setPopup();
-  naver_id_login.init_naver_id_login();
-};
+//const initNaverLogin = () => {
+//  var naver_id_login = new window.naver_id_login(
+//    "nuZ04sTeb2LDphCqc4qv", // client ID
+//    "http://localhost:3000/" // callback url
+//  );
+//  var state = naver_id_login.getUniqState();
+//  naver_id_login.setButton("white", 2, 40);
+//  naver_id_login.setDomain("http://localhost:3000");
+//  naver_id_login.setState(state);
+//  //naver_id_login.setPopup();
+//  naver_id_login.init_naver_id_login();
+//};
 
 const Login = () => {
-  useEffect(() => {
-    initNaverLogin();
-  }, []);
-
   return (
     <>
       <div>Hello</div>
-      <div id="naver_id_login"></div>
+      <div id="naverIdLogin"></div>
     </>
   );
 };
