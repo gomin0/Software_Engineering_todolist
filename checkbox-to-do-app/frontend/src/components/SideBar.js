eae195ea7f03b8715803af276b97d40372f1a5a0
WooSeok Kim <3suksw@gmail.com>
2023-05-28 01:04:09 +0900

    showing List Modal

--- checkbox-to-do-app/frontend/src/components/SideBar.js
+++ checkbox-to-do-app/frontend/src/components/SideBar.js
@@ -1,21 +1,24 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import style from "./SideBar.css";
 import List from "./List";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import ListModal from "./ListModal";
 
 const SideBar = ({ onClick, lists, selected }) => {
   const curLists = lists?.sort((a, b) => a.createdDate - b.createdDate);
+  const [showModal, setShowModal] = useState(false);
 
   // NOTE: search bar
   return (
     <div className="sidebar" style={style}>
       <div className="list-header">
         <h3>Lists</h3>
-        <button className="add-list" onClick={() => alert("clicked")}>
+        <button className="add-list" onClick={() => setShowModal(true)}>
           <FontAwesomeIcon icon={faPlus} className="add-list-btn" />
         </button>
       </div>
+      {showModal && <ListModal mode={"Create"} setShowModal={setShowModal} />}
       <div className="list-container">
         <ul className="lists">
           {curLists?.map((list) => (
