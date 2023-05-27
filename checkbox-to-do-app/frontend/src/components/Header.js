fb09caa9364b7d84c66aa72271785687258cff9c
WooSeok Kim <3suksw@gmail.com>
2023-05-27 15:18:15 +0900

    modify prop type for user info

--- checkbox-to-do-app/frontend/src/components/Header.js
+++ checkbox-to-do-app/frontend/src/components/Header.js
@@ -1,19 +1,15 @@
 import style from "./Header.css";
 
-const Header = ({ user }) => {
-  const myAlert = (user) => {
-    console.log(user.userName);
-  };
-
+const Header = ({ curUser }) => {
   return (
     <div className="header" style={style}>
       <div className="header-left">✓ Checkbox</div>
       <div className="user-info">
         <button
           className="user-icon"
-          onClick={() => alert(user.userName)}
+          onClick={() => alert(curUser.username)}
         ></button>
-        <p>{user.userName}</p>
+        <p>{curUser.username}</p>
       </div>
     </div>
   );
