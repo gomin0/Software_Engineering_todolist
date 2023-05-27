793d690a1a375307e8d020b7c84319ff97a6742b
WooSeok Kim <3suksw@gmail.com>
2023-05-27 15:17:20 +0900

    add button for lists

--- checkbox-to-do-app/frontend/src/components/SideBar.js
+++ checkbox-to-do-app/frontend/src/components/SideBar.js
@@ -1,6 +1,8 @@
 import React, { useState, useRef } from "react";
 import style from "./SideBar.css";
 import List from "./List";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 const SideBar = ({ onClick, lists, selected }) => {
   const curLists = lists?.sort((a, b) => a.createdDate - b.createdDate);
@@ -8,6 +10,12 @@ const SideBar = ({ onClick, lists, selected }) => {
   // NOTE: search bar
   return (
     <div className="sidebar" style={style}>
+      <div className="list-header">
+        <h3>Lists</h3>
+        <button className="add-list" onClick={() => alert("clicked")}>
+          <FontAwesomeIcon icon={faPlus} className="add-list-btn" />
+        </button>
+      </div>
       <div className="list-container">
         <ul className="lists">
           {curLists?.map((list) => (
