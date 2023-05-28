88d3cee8914b611fa92322e6f4b58757c2078eb6
WooSeok Kim <3suksw@gmail.com>
2023-05-28 22:10:43 +0900

    comment out selection

--- checkbox-to-do-app/frontend/src/components/Main.js
+++ checkbox-to-do-app/frontend/src/components/Main.js
@@ -6,11 +6,15 @@ import Inbox from "./Inbox";
 const Main = ({ curUser, lists }) => {
   var initialList = lists[0];
   var [curList, setListName] = useState(initialList);
+  const [selection, setSelect] = useState(0);
 
   const clickAndSelectList = (event) => {
-    const selectedID = event.target.id;
-    const selectedList = lists.find((list) => list.id === parseInt(selectedID));
-    setListName(selectedList);
+    //const selectedID = event.target.id;
+    //const selectedList = lists.find((list) => list.id === parseInt(selectedID));
+    //setListName(selectedList);
+    const select = event.target.id;
+    console.log(select);
+    setSelect(selection);
   };
 
   return (
