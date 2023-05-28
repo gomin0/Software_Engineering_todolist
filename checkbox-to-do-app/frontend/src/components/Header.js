01fc7fd1b0153a3f64d1fae1ad0d82542d882021
WooSeok Kim <3suksw@gmail.com>
2023-05-29 03:52:59 +0900

    add sign out feature

--- checkbox-to-do-app/frontend/src/components/Header.js
+++ checkbox-to-do-app/frontend/src/components/Header.js
@@ -1,3 +1,5 @@
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 import style from "./Header.css";
 
 const Header = ({ curUser }) => {
@@ -6,6 +8,28 @@ const Header = ({ curUser }) => {
     "--background-image",
     `url(${curUser.profileImageURL})`
   );
+
+  const navigate = useNavigate();
+
+  const NaverLogout = async () => {
+    try {
+      const response = await axios.get("/oauth2.0/token", {
+        params: {
+          grant_type: "delete",
+          client_id: "nuZ04sTeb2LDphCqc4qv",
+          client_secret: "14kXprQq98",
+          access_token: curUser.access_token,
+          service_provider: "NAVER",
+        },
+      });
+      if (response) {
+        console.log(response);
+        navigate("/login");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <div className="header" style={style}>
       <div className="header-left">✓ Checkbox</div>
@@ -15,6 +39,9 @@ const Header = ({ curUser }) => {
           onClick={() => alert(curUser.username)}
         ></button>
         <p>{curUser.username}</p>
+        <div className="naverLogout" onClick={() => NaverLogout()}>
+          Sign out
+        </div>
       </div>
     </div>
   );
