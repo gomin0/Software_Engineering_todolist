1c02a6dd34991c62a0ce88ac657acf97f8ad558a
jinho7 <jh7524jh7524@gmail.com>
2023-06-18 03:51:52 +0900

    login butten fix

--- checkbox-to-do-app/frontend/src/routes/Login.js
+++ checkbox-to-do-app/frontend/src/routes/Login.js
@@ -11,7 +11,7 @@ const NaverLogin = () => {
       clientId: NAVER_CLIENT_ID,
       callbackUrl: NAVER_CALLBACK_URL,
       isPopup: false,
-      loginButton: { color: "green", type: 3, height: 58 },
+      loginButton: { color: "white", type: 3, height: 58 },
       callbackHandle: true,
     });
     naverLogin.init();
